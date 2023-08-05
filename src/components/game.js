957d2f1d6c9cc12ce55d612e84a7b12106e5b34d
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-08-05 17:41:35 -0500

    answer hidden/shown add

--- src/components/game.js
+++ src/components/game.js
@@ -2,26 +2,40 @@ import {state, useState} from 'react'
 
 const Game = (props) => {
 
-    const [ game, gameHandler ] = useState("")
+    const [ game, setGame ] = useState(undefined)
+
+    const [gameAnswer, setAnswer ] = useState("")
 
     // fetch from api
     const fetchData = async () => {
         const url = 'http://jservice.io/api/random';
         const response = await fetch(url)
         const game = await response.json()
+        setGame(game[0])
         console.log(game[0].question)
+        setAnswer('')
+        // return <div>
+        //     <h1>Answer:{game[0].answer}</h1>
+        // </div>
     }
+
+    const showAnswer = () => {
+        const answer = game.answer
+        console.log(answer)
+        setAnswer(answer)
+    }
+    
 
     return <div className="game">
         <h2>Let's Play!</h2>
         <div className="answer">
-            <button onClick={(fetchData)}>New Answer</button>
-            <h3>Category:</h3>
-            <h3>Points:</h3>
-            <h3>Answer:</h3>
-        </div>
-        <div className="question">
-            <button>Click to Reveal Question</button>
+            <button onClick={(fetchData)}>New Question</button>
+            <h3>Category: {game && game.category.title}</h3>
+            <h3>Points: {game && game.value}</h3>
+            <h3>Question: {game && game.question}</h3>
+    
+            <button onClick={(showAnswer)}>Click to Reveal Answer</button>
+            <h3>Answer: {gameAnswer}</h3>
         </div>
     </div>
 }
